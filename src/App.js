4999a4a3a0f86b9896e7d79f079073bbb8b0f452
nerdjot <njsingh3948@gmail.com>
2021-11-22 23:32:54 +0530

    made cocktail wiht lower parallax animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Cocktail from './components/Cocktail.js';
 import './App.css';
 export default function App() {
   return (
-    
     <div className="app">
       <Navbar />
-      <Home />
+      <Cocktail />
     </div>
   );
 }
---
+++ src/components/Cocktail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/Cocktail.css';
+const Cocktail = () => {
+  return (
+    <div className="cocktail-div">
+      <div className="upper-div"></div>
+      <div className="lower-paralax-div">
+        <div className="parallax-img">
+          <div className="sample-div-1"></div>
+          <div className="sample-div-2"></div>
+        </div>
+        <div className="parallax-img">
+          <div className="sample-div-1"></div>
+          <div className="sample-div-2"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Cocktail;
