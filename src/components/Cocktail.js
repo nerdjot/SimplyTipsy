076133eb8472cf86d0333f93fa8c7d8e54fccaaa
nerdjot <njsingh3948@gmail.com>
2021-11-22 23:43:19 +0530

    animation without scroll

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -4,14 +4,16 @@ const Cocktail = () => {
   return (
     <div className="cocktail-div">
       <div className="upper-div"></div>
-      <div className="lower-paralax-div">
-        <div className="parallax-img">
-          <div className="sample-div-1"></div>
-          <div className="sample-div-2"></div>
-        </div>
-        <div className="parallax-img">
-          <div className="sample-div-1"></div>
-          <div className="sample-div-2"></div>
+      <div className="lower-div">
+        <div className="lower-paralax-div">
+          <div className="parallax-img">
+            <div className="sample-div-1"></div>
+            <div className="sample-div-2"></div>
+          </div>
+          <div className="parallax-img">
+            <div className="sample-div-1"></div>
+            <div className="sample-div-2"></div>
+          </div>
         </div>
       </div>
     </div>
